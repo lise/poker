90b26a8368b84edd04cc3a1b0b11ff676116f322
unknown <Lise Engmo@.BEKK.no>
2010-01-08 13:42:44 +0100

    lagt til pokergame klassen

---
+++ PokerGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Poker
+{
+    public class PokerGame
+    {
+        private string Input;
+
+        public PokerGame(string input)
+        {
+            this.Input = input;
+        }
+
+        public string findWinner()
+        {
+            //TODO gjør noe lurt
+            return Input;
+        }
+    }
+}
