90d93075d17e66f6ffcfa2f258c8d1ac0792e95d
unknown <Lise Engmo@.BEKK.no>
2010-01-08 15:47:26 +0100

    new project

---
+++ PokerconsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Poker;
+
+namespace PokerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PokerGame game = new PokerGame();
+            Console.ReadLine();
+        }
+    }
+}
